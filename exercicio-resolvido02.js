ed86dbef3f323bd589f9c4984534f9d2830635eb
unknown <evandro.ucciolini@outlook.com>
2023-09-07 23:22:57 -0300

    Retificação dos dados de habitantes de cidade de São Paulo

--- exercicio-resolvido02.js
+++ exercicio-resolvido02.js
@@ -1,7 +1,7 @@
-let populacao = 45
+let populacao = 11_253_503
 
-console.log(`Em 2022, o número de habitantes em São Paulo era de aproximadamente: ${populacao} milhões.`)
+console.log(`Em 2010, o número de habitantes em São Paulo era de: ${populacao}.`)
 
-populacao = 50
+populacao = 11_451_245
 
-console.log(`Agora em 2023, o número de habitantes atingiu aproximadamente ${populacao} milhões.`) 
+console.log(`Agora em 2023, o número de habitantes atingiu ${populacao}.`) 
